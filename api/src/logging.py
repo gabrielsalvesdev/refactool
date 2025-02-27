b16995eaa3938bf685956cbaf351d152aae38b65
Gabriel Alves <contato@gabrielsousa.dev>
2025-02-27 20:02:06 -0300

    fix: Corrige configuração do structlog para usar contextvars corretamente

--- api/src/logging.py
+++ api/src/logging.py
@@ -1,15 +1,17 @@
 # api/src/logging.py
 import json
 import structlog
+from structlog.contextvars import merge_contextvars
 
 structlog.configure(
     processors=[
+        merge_contextvars,
         structlog.processors.JSONRenderer(
             serializer=lambda data, **kwargs: json.dumps(data, ensure_ascii=False)
         ),
         structlog.processors.add_log_level
     ],
-    context_class=structlog.contextvars.wrap_dict(dict),
+    context_class=dict,
 )
 
 logger = structlog.get_logger() 
